2d2c446eb1fbbb9ea4a350073e362184f14850f0
Serhat Tomak <serhattomak@gmail.com>
2024-02-26 17:31:53 +0300

    Service

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -31,7 +31,7 @@ namespace NLayer.Repository.Repositories
         {
             return await _dbSet.AnyAsync(expression);
         }
-        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        public IQueryable<T> GetAll()
         {
             return _dbSet.AsNoTracking().AsQueryable();
         }
