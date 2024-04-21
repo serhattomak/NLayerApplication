31cd61f7a36f4fa7a6ad701966c0cfe65803689e
Serhat Tomak <serhattomak@gmail.com>
2024-04-21 15:20:56 +0300

    GetCategoryByIdWithProducts Endpoint

---
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+
+namespace NLayer.Repository.Repositories
+{
+    public class CategoryRepository:GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetSingleCategoryByIdWithProductAsync(int categoryId)
+        {
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
