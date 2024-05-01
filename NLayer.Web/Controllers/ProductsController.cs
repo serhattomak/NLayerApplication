fed9e9e1e793b60cef46072763bc8716383af582
Serhat Tomak <serhattomak@gmail.com>
2024-05-01 16:48:16 +0300

    ProductController-1

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IProductService _services;
+    private readonly IMapper _mapper;
+
+    public ProductsController(IProductService services, IMapper mapper)
+    {
+        _services = services;
+        _mapper = mapper;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _services.GetProductsWithCategory());
+    }
+
+}
