1ca1b754f500d6292c3efdeba5de1df2747a9393
Serhat Tomak <serhattomak@gmail.com>
2024-05-05 19:49:09 +0300

    ProductApiService/CategoryApiService

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -8,6 +8,7 @@ using NLayer.Web.Modules;
 using System.Reflection;
 using NLayer.Service.Validations;
 using NLayer.Web;
+using NLayer.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,16 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     {
         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
     });
+});
+
+builder.Services.AddHttpClient<ProductApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
+});
+
+builder.Services.AddHttpClient<CategoryApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
 });
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
---
+++ NLayer.Web/Services/CategoryApiService.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayer.Web.Services;
+
+public class CategoryApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public CategoryApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+}
